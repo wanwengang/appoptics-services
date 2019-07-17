f7b470ffdb035c75f7063b8072142cf19c69e854
gavin <wengang.wan@zoom.us>
2019-07-17 14:34:12 +0800

    zoom-notification

---
+++ services/zoom.rb
@@ -0,0 +1,89 @@
+# encoding: utf-8
+
+require 'uri'
+require 'cgi'
+require 'yajl'
+require 'faraday'
+
+module AppOptics::Services
+  class Service::Zoom < AppOptics::Services::Service
+    def receive_validate(errors = {})
+      if settings[:url].to_s.empty?
+        errors[:url] = "Is required"
+        return false
+      end
+
+      uri = URI.parse(settings[:url]) rescue nil
+      unless uri
+        errors[:url] = "Is not a valid URL"
+        return false
+      end
+
+      # Catches bare hostnames or IPs such as www.example.com or 192.0.2.0
+      if !uri.scheme
+        errors[:url] = 'Is missing protocol'
+        return false
+      end
+
+      return true
+    end
+
+    def account_email
+      if payload['auth']
+        payload['auth']['email']
+      end
+    end
+
+    def receive_alert_clear
+      raise_config_error unless receive_validate({})
+      uri = URI.parse(settings[:url])
+      result = {
+        :alert => payload['alert'],
+        :account => account_email,
+        :trigger_time => payload['trigger_time'],
+        :clear => "normal",
+        :incident_key => payload['incident_key']
+      }
+      post_it(uri, result)
+    end
+
+    def receive_alert
+      raise_config_error unless receive_validate({})
+	  
+	  raise_config_error('Zoom does not support V1 alerts') unless payload[:alert][:version] == 2
+	  
+      uri = URI.parse(settings[:url])
+
+      result = {
+        :alert => payload['alert'],
+        :account => account_email,
+        :trigger_time => payload['trigger_time'],
+        :incident_key => payload['incident_key'],
+        :conditions => payload['conditions'],
+        :violations => payload['violations'],
+        :triggered_by_user_test => payload['triggered_by_user_test']
+      }
+      
+      post_it(uri, result)
+    end
+
+    def post_it(uri, hash)
+      # Faraday doesn't unescape user and password
+      if uri.userinfo
+        http.basic_auth *uri.userinfo.split(":").map{|x| CGI.unescape(x)}
+      end
+      url = "%s://%s:%d%s" % [uri.scheme, uri.host, uri.port, uri.request_uri]
+      http_post url, {:payload => Yajl::Encoder.encode(hash)}
+    rescue Faraday::Error::ConnectionFailed
+      log("Connection failed for url: #{url} for payload: #{payload.inspect}")
+    end
+
+    def log(msg)
+      if defined?(Rails)
+        Rails.logger.info(msg)
+      else
+        puts(msg)
+      end
+    end
+  end
+end
---
+++ test/zoom_test.rb
@@ -0,0 +1,90 @@
+require File.expand_path('../helper', __FILE__)
+
+module AppOptics::Services
+  class ZoomTest < AppOptics::Services::TestCase
+    def setup
+      @stubs = Faraday::Adapter::Test::Stubs.new
+    end
+
+    def test_validations
+      svc = service(:alert, {:url => "http://foobar.com/push"}, alert_payload)
+      errors = {}
+      assert(svc.receive_validate(errors))
+      assert_equal(0, errors.length)
+
+      svc = service(:alert, {}, alert_payload)
+      errors = {}
+      assert(!svc.receive_validate(errors))
+      assert_equal(1, errors.length)
+      assert(!errors[:url].nil?)
+
+      svc = service(:alert, {:url => "http://user@@foobar.com:/blah"}, alert_payload)
+      errors = {}
+      assert(!svc.receive_validate(errors))
+      assert_equal(1, errors.length)
+      assert(!errors[:url].nil?)
+
+      svc = service(:alert, {:url => "example.com/foo"}, alert_payload)
+      errors = {}
+      assert(!svc.receive_validate(errors))
+      assert_equal(1, errors.length)
+      assert(!errors[:url].nil?)
+    end
+
+    def test_alerts
+      path = "/post_path.json"
+      url = "http://localhost#{path}"
+      svc = service(:alert, { :url => url }, alert_payload)
+
+      @stubs.post "#{path}" do |env|
+        [200, {}, '']
+      end
+
+      svc.receive_alert
+    end
+
+    def test_new_alerts
+      path = "/post_path.json"
+      url = "http://localhost#{path}"
+      svc = service(:alert, { :url => url }, new_alert_payload)
+
+      @stubs.post "#{path}" do |env|
+        payload = JSON.parse(env[:body][:payload])
+        assert_equal ["account", "alert", "conditions", "incident_key", "trigger_time", "triggered_by_user_test", "violations"], payload.keys.sort
+        assert_equal 123, payload['alert']['id']
+        assert_equal 'Some alert name', payload['alert']['name']
+        assert_equal 1, payload['conditions'].length
+        assert_equal "foo@example.com", payload['account']
+        assert_equal false, payload['triggered_by_user_test']
+        assert_equal 'foo', payload['incident_key']
+        violations = payload['violations']
+        foo_bar_violations = violations['foo.bar']
+        assert_equal 1, foo_bar_violations.length
+        assert_equal 'metric.name', foo_bar_violations[0]['metric']
+        [200, {}, '']
+      end
+
+      svc.receive_alert
+    end
+
+    def test_new_alert_test_trigger
+      path = "/post_path.json"
+      url = "http://localhost#{path}"
+      payload = new_alert_payload.dup
+      payload[:triggered_by_user_test] = true
+      svc = service(:alert, { :url => url }, payload)
+
+      @stubs.post "#{path}" do |env|
+        payload = JSON.parse(env[:body][:payload])
+        assert_equal true, payload['triggered_by_user_test']
+        [200, {}, '']
+      end
+
+      svc.receive_alert
+    end
+
+    def service(*args)
+      super AppOptics::Services::Service::Zoom, *args
+    end
+  end
+end
